98330ed31ebdb02ef4e685335adef5561a0c7a83
Esteban Sanabria <est.juane.sanabria@unimilitar.edu.co>
2026-03-27 20:21:16 -0500

    Creación de metodos en clase aereopuerto para registrar pasajero y modificar estado del pasajero

--- src/main/java/Aereopuerto.java
+++ src/main/java/Aereopuerto.java
@@ -1,21 +1,20 @@
+
+import com.murcia.utils.Input;
+
 //Juan Esteban Sanabria - 6014495, Katherine Ortega Contreras - 6014550
-
-
-
 public class Aereopuerto {
 
 private String nombre;
 private Vuelo vuelo;
 private Pasajero pasajero;
 private Avion avion;
-    public static void main(String[] args) {
-    }
+    
           // Registrar vuelo
     public void registrarVuelo(Vuelo vuelo) {
         this.vuelo = vuelo;
         System.out.println("Vuelo registrado: " + vuelo.getNumeroVuelo());// void
     }
-
+    
     // Modificar estado del vuelo
     public void modificarEstadoVuelo(String estado) {
         if (vuelo != null) {
@@ -25,7 +24,22 @@ private Avion avion;
             System.out.println("No hay vuelo registrado.");
         }
     }
+    //registrar pasajero
+    public void registrarPasajero(Pasajero pasajero) {
+        this.pasajero = pasajero;
+        System.out.println("Pasajero registrado: " + pasajero.getNombre());
+        
+    //Modificasr estado del pasajero    
+        public void modificarEstadoPasajero(String estado) {
+        if (pasajero != null) {
+            pasajero.cambiarEstado(estado);
+            System.out.println("Estado del pasajero cambiado a: " + estado);
+        } else {
+            System.out.println("No hay pasajero registrado.");
+        }
+    }
 
+//TODO VA CON LISTAS ENLAZADAS
 //los metodos a usar en la clase aereopuerto son los siguientes:
 //registrar vuelo, modificar vuelo
 //registrar pasajero, modificar pasajero
